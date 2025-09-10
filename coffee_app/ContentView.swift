5ffb27c871c1c679846c99fc7e045213763d43c0
Nagares, Wayne <nagares.wayne.yzon.d@gmail.com>
2025-09-10 15:11:58 +0800

    Initial Commit

---
+++ coffee_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coffee_app
+//
+//  Created by STUDENT on 9/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coffee_app/coffee_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_appApp.swift
+//  coffee_app
+//
+//  Created by STUDENT on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct coffee_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
