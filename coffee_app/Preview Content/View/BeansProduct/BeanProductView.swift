e910368017e33e38e71a2ff25ca4466175fbc500
Vergara Mharon Irvin <mharonirvin@gmail.com>
2025-11-14 15:22:10 +0800

    saved nov-14 15:22

--- coffee_app/Preview Content/View/BeansProduct/BeanProductView.swift
+++ coffee_app/Preview Content/View/BeansProduct/BeanProductView.swift
@@ -31,142 +31,140 @@ struct BeanProductView: View {
         self.beanProduct = beanProduct
     }
     
+    
+    @State var selectedRoastType: Int = 0
+    @State var selectedSize: Int = 0
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(alignment: .leading) {
+            ZStack {
+                ScrollView {
                     Image("bean_product")
                         .resizable()
                         .frame(maxWidth: .infinity)
                         .scaledToFit()
                         .padding(.top, 50)
                     
-                    HStack {
-                        TextPageTitle(text: beanProduct.name)
-                        Spacer()
-                        Image(systemName: "heart")
-                            .foregroundStyle(.red)
-                            .font(.system(size: 20))
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom, 5)
-                    
-                    HStack {
-                        Image(systemName: "star.fill")
-                            .foregroundStyle(.yellow)
-                            .font(.system(size: 12))
-                        Text("\(String(format: "%.1f", beanProduct.rating)) / 5")
-                            .font(.system(size: 14, weight: .light, design: .serif))
-                            .foregroundStyle(ThemeColor.green)
-                            .padding(.trailing, 15)
-                        Text("\(Utils.formatAsK(number: beanProduct.numOfSold))+ sold")
-                            .font(.system(size: 14, weight: .light, design: .serif))
-                            .foregroundStyle(.blue)
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom, 2)
-                    
-                    Text("₱ \(String(format: "%.2f", beanProduct.price))")
-                        .font(.system(size: 28, weight: .semibold, design: .serif))
-                        .foregroundStyle(ThemeColor.brown)
-                        .padding(.horizontal)
-                        .padding(.bottom, 30)
-                    
-                    
-                    TextSection(text: "Available Roast Types")
-                        .padding(.horizontal)
-                    // ROAST TYPES WITH PLACEHOLDER VALUES (SEE TOP)
-                    GridBuilderView(items: roastTypes, columns: 2, content: { roastType in
-                        WhiteBorderButton(
-                            text: roastType.type,
-                            width: 180
-                        )
-                    }, hSpacing: 5, vSpacing: 8)
-                    .padding(.bottom, 25)
-                    
-                    
-                    TextSection(text: "Sizes")
-                        .padding(.horizontal)
-                    // SIZES WITH PLACEHOLDER VALUES (SEE TOP)
-                    GridBuilderView(items: sizes, columns: 3, content: { size in
-                        WhiteBorderButton(
-                            text: size.size,
-                            width: 115
-                        )
-                    }, hSpacing: 0, vSpacing: 10)
-                    .padding(.bottom, 30)
-                    
-                    Divider()
-                        .background(.white)
-                        .padding(.bottom, 30)
-                        .padding(.horizontal)
-                    
-                    TextSection(text: "Description")
-                        .padding(.horizontal)
-                        .padding(.bottom, 15)
-                    TextParagraph(text:
-                        """
-                        This is where the description of the farm or shop goes. This is where they will describe all about their farm, the products they produce, or whatever they may be inclined to put in this section
-                        
-                        Additional things to say or describe a product will be here. It will be up to the shop or farm to decide what descriptions and product specifications they will include. Just that this section is for that purpose
-                        
-                        One last paragraph to demonstrate this section of the page. This is where descriptions will go about the product. More may be said depending on the farm and the descriptions and information they include. It is up to them.
-                        """
-                    )
-                    .padding(.bottom, 30)
-                    
-                    Divider()
-                        .background(.white)
-                        .padding(.bottom, 10)
-                        .padding(.horizontal)
-                    
-                    HStack {
-                        Image("farm_bg")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                            .padding(.trailing, 10)
-                        
+                    Group {
                         VStack(alignment: .leading) {
-                            Text("Farm Name Goes Here")
-                                .foregroundStyle(.white)
-                                .font(.system(size: 13, weight: .medium, design: .serif))
-                            Text("Location of Farm")
-                                .foregroundStyle(.gray)
-                                .font(.system(size: 13, weight: .light, design: .serif))
-                        }
-                        
-                        Spacer()
-                        
-                        VStack {
-                            Text("5.0")
-                                .foregroundStyle(.white)
-                                .font(.system(size: 13, weight: .medium, design: .serif))
-                            Text("Rating")
-                                .foregroundStyle(.gray)
-                                .font(.system(size: 12, weight: .medium, design: .serif))
-                            WhiteBorderButton(
-                                text: "Visit",
-                                width: 70,
-                                textSize: 12,
-                                verticalPadding: 5
+                            HStack {
+                                TextPageTitle(text: beanProduct.name)
+                                Spacer()
+                                Image(systemName: "heart")
+                                    .foregroundStyle(.red)
+                                    .font(.system(size: 20))
+                            }
+                            .padding(.bottom, 5)
+                            
+                            HStack {
+                                Image(systemName: "star.fill")
+                                    .foregroundStyle(.yellow)
+                                    .font(.system(size: 12))
+                                Text("\(String(format: "%.1f", beanProduct.rating)) / 5")
+                                    .font(.system(size: 14, weight: .light, design: .serif))
+                                    .foregroundStyle(ThemeColor.green)
+                                    .padding(.trailing, 15)
+                                Text("\(Utils.formatAsK(number: beanProduct.numOfSold))+ sold")
+                                    .font(.system(size: 14, weight: .light, design: .serif))
+                                    .foregroundStyle(.blue)
+                            }
+                            .padding(.bottom, 2)
+                            
+                            Text("₱ \(String(format: "%.2f", beanProduct.price))")
+                                .font(.system(size: 28, weight: .semibold, design: .serif))
+                                .foregroundStyle(ThemeColor.brown)
+                                .padding(.bottom, 30)
+                            
+                            
+                            TextSection(text: "Available Roast Types")
+                            // ROAST TYPES WITH PLACEHOLDER VALUES (SEE TOP)
+                            GridBuilderView(items: roastTypes, columns: 2, content: { roastType in
+                                WhiteBorderButton(
+                                    text: roastType.type,
+                                    width: 180
+                                )
+                            }, hSpacing: 5, vSpacing: 8, hPadding: 0)
+                            .padding(.bottom, 25)
+                            
+                            
+                            TextSection(text: "Sizes")
+                            // SIZES WITH PLACEHOLDER VALUES (SEE TOP)
+                            GridBuilderView(items: sizes, columns: 3, content: { size in
+                                WhiteBorderButton(
+                                    text: size.size,
+                                    width: 115
+                                )
+                            }, hSpacing: 0, vSpacing: 10, hPadding: 0)
+                            .padding(.bottom, 30)
+                            
+                            Divider()
+                                .background(.white)
+                                .padding(.bottom, 30)
+                            
+                            TextSection(text: "Description")
+                                .padding(.bottom, 15)
+                            TextParagraph(text:
+                                """
+                                This is where the description of the farm or shop goes. This is where they will describe all about their farm, the products they produce, or whatever they may be inclined to put in this section
+                                
+                                Additional things to say or describe a product will be here. It will be up to the shop or farm to decide what descriptions and product specifications they will include. Just that this section is for that purpose
+                                
+                                One last paragraph to demonstrate this section of the page. This is where descriptions will go about the product. More may be said depending on the farm and the descriptions and information they include. It is up to them.
+                                """
                             )
-                        }
-                        .padding(.trailing)
-                
-                    }
+                            .padding(.bottom, 30)
+                            
+                            Divider()
+                                .background(.white)
+                                .padding(.bottom, 10)
+                            
+                            HStack {
+                                Image("farm_bg")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 50, height: 50)
+                                    .clipShape(Circle())
+                                    .padding(.trailing, 10)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Farm Name Goes Here")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 13, weight: .medium, design: .serif))
+                                    Text("Location of Farm")
+                                        .foregroundStyle(.gray)
+                                        .font(.system(size: 13, weight: .light, design: .serif))
+                                }
+                                
+                                Spacer()
+                                
+                                VStack {
+                                    Text("5.0")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 13, weight: .medium, design: .serif))
+                                    Text("Rating")
+                                        .foregroundStyle(.gray)
+                                        .font(.system(size: 12, weight: .medium, design: .serif))
+                                    WhiteBorderButton(
+                                        text: "Visit",
+                                        width: 70,
+                                        textSize: 12,
+                                        verticalPadding: 5,
+                                        isOption: false
+                                    )
+                                }
+                                .padding(.trailing)
+                        
+                            }
+                            .padding(.bottom, 10)
+                            
+                            Divider()
+                                .background(.white)
+                                .padding(.bottom, 30)
+                            
+                            TextSection(text: "You Might Like")
+                        } // Vstack
+                    } // Group
                     .padding(.horizontal)
-                    .padding(.bottom, 10)
-                    
-                    Divider()
-                        .background(.white)
-                        .padding(.bottom, 30)
-                        .padding(.horizontal)
-                    
-                    TextSection(text: "You Might Like")
-                        .padding(.horizontal)
-                    // THIS IS A PLACEHOLDER FOR THE SLIDER
                     ImageSliderView(items: Controller.beanProducts, automaticScrolling: false, content: { beanProduct in
                         BeanProductCard(
                             image: beanProduct.image,
@@ -178,12 +176,48 @@ struct BeanProductView: View {
                             imageScale: 0.7
                         )
                     }, hSpacing: 20)
-                    .padding(.bottom, 20)
+                    .padding(.bottom, 80)
+                } // ScrollView
+                
+                VStack {
+                    Spacer()
                     
-                } // Vstack
-            } // ScrollView
+                    HStack {
+                        Button(action: {
+                            // Handle Add to Cart action
+                            print("Add to Cart tapped")
+                        }) {
+                            Text("Add to Cart")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity) // Take up equal space
+                                .padding()
+                                .background(ThemeColor.brown)
+                        }
+                        
+                        Button(action: {
+                            print("Buy Now tapped")
+                        }) {
+                            Text("Buy Now")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity) // Take up equal space
+                                .padding()
+                                .background(ThemeColor.green)
+                        }
+                    }
+                    .padding(.bottom, 20) // Space from the bottom edge
+                    .background(Color.black) // Tab bar background color
+                    .shadow(radius: 10) // Floating effect shadow
+                    .padding(.bottom, 15)
+                }
+                .ignoresSafeArea(edges: .bottom)
+                
+            } // ZStack
             .background(.black)
             .edgesIgnoringSafeArea(.top)
+            
+            
             
         } // NavigationStack
         .toolbarBackground(.black, for: .navigationBar)
--- coffee_app/Preview Content/View/BeansProduct/WhiteBorderButton.swift
+++ coffee_app/Preview Content/View/BeansProduct/WhiteBorderButton.swift
@@ -5,28 +5,67 @@ struct WhiteBorderButton: View {
     var width: CGFloat
     var textSize: CGFloat
     var verticalPadding: CGFloat
+    var isOption: Bool = true
+    @State var isPressed: Bool = false
     
-    init(text: String, width: CGFloat, textSize: CGFloat = 12, verticalPadding: CGFloat = 8) {
+    init(text: String, width: CGFloat, textSize: CGFloat = 12, verticalPadding: CGFloat = 8, isOption: Bool = true) {
         self.text = text
         self.width = width
         self.textSize = textSize
         self.verticalPadding = verticalPadding
+        self.isOption = isOption
     }
 
     var body: some View {
-        Button(action: {
-            print("Button Pressed")
-        }) {
-            Text(text)
-                .font(.system(size: textSize, weight: .light, design: .serif))
-                .foregroundStyle(.white)
-                .lineLimit(1)
-                .padding(.vertical, verticalPadding)
-                .frame(width: width)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 4)
-                        .stroke(.white, lineWidth: 1)
-                )
+        if isOption {
+            Button(action: {
+                print(text)
+                if isPressed {
+                    isPressed = false
+                } else {
+                    isPressed = true
+                }
+            }) {
+                if isPressed {
+                    Text(text)
+                        .font(.system(size: textSize, weight: .light, design: .serif))
+                        .foregroundStyle(.black)
+                        .lineLimit(1)
+                        .padding(.vertical, verticalPadding)
+                        .frame(width: width)
+                        .background(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(.white, lineWidth: 1)
+                        )
+                } else {
+                    Text(text)
+                        .font(.system(size: textSize, weight: .light, design: .serif))
+                        .foregroundStyle(.white)
+                        .lineLimit(1)
+                        .padding(.vertical, verticalPadding)
+                        .frame(width: width)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(.white, lineWidth: 1)
+                        )
+                }
+            }
+        } else {
+            Button(action: {
+                print(text)
+            }) {
+                Text(text)
+                    .font(.system(size: textSize, weight: .light, design: .serif))
+                    .foregroundStyle(.white)
+                    .lineLimit(1)
+                    .padding(.vertical, verticalPadding)
+                    .frame(width: width)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(.white, lineWidth: 1)
+                    )
+            }
         }
     }
 }
